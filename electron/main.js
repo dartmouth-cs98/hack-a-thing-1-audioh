ff9d360434100d919df7a658889db5a1c55513e2
ijemmao <ijemmao>
2019-01-10 01:07:37 -0500

    included Electron app

---
+++ electron/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+let mainWindow
+
+require('electron-reload')(__dirname + '/index.html', {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
